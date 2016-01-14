cd2af60ccb3eb342ec6bf2141a62a001748967a6
David Kelly <thedavidkelly@hotmail.com>
2016-01-14 10:47:31 +0000

    added some tests

--- tests/unit-tests/controllers.js
+++ tests/unit-tests/controllers.js
@@ -3,8 +3,14 @@ describe("Controller Unit Tests", function() {
     beforeEach(angular.mock.module('starter'));
     var scope;
     var controller;
+    var state;
 
-    beforeEach(inject(function($rootScope, $controller) {
+    /**
+     * General set up of root scope, and a simple test of DocsCtrl
+     */
+    beforeEach(inject(function($rootScope, $controller, $state) {
+        state = $state;
+        spyOn(state, 'go');
         scope = $rootScope.$new();
         controller = $controller('DocsCtrl', {
             $scope: scope,
@@ -17,5 +23,31 @@ describe("Controller Unit Tests", function() {
 
     it('should have a scope defined', function() {
         expect(scope).toBeDefined();
+    });
+
+    it('should have DocsCtrl defined', function() {
+        expect(controller).toBeDefined();
+    });
+
+    it('should move to status page when button is pressed', function() {
+        scope.goBack();
+        expect(state.go).toHaveBeenCalled();
+    });
+
+
+    /**
+     * tests for the About Controller
+     */
+    beforeEach(inject(function($controller, $state) {
+        state = $state;
+        controller = $controller('AboutCtrl', {
+            $scope: scope,
+        });
+    }));
+
+    // this test is not yet complete
+    it('should open a link in the browser', function() {
+        var link = 'http://www.google.com';
+        scope.goToLink(link);
     });
 });
