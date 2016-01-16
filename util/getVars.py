6ec3fd859e5380d2096e23e8e2340f6444d23c92
David Kelly <thedavidkelly@hotmail.com>
2016-01-16 22:32:18 +0000

    test of re.compile

--- util/getVars.py
+++ util/getVars.py
@@ -2,6 +2,7 @@
 ## simple python script to lift all the $scopes and vars from a .js file
 
 import sys
+import re
 
 fileString = str(sys.argv[1])
 
@@ -11,16 +12,16 @@ def storeLine(line):
     variables.append(line)
 
 f = None
+scope = re.compile("$scope*")
 try:
     f= open(fileString, 'r')
-    for line in enumerate(f):
-        print(line)
-        if line.strip().startswith('$scope'):
+    for line in f:
+        if scope.match(line):
+            print("found")
             storeLine(line)
-
-    print(len(variables))
+            variables.append(line)
 except:
     print("error")
 finally:
-    if f != None:
+     if f != None:
         f.close()
